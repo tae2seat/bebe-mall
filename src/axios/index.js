9d788587b10ab2c4bd2e51d3c7f3a2b2f5f2aaa6
김혜영 <kimhyeyeong@gimhyeyeong-ui-MacBookAir.local>
2023-06-02 15:56:29 +0900

    axios interceptor

--- src/axios/index.js
+++ src/axios/index.js
@@ -4,4 +4,34 @@ export const authApi = axios.create({
     baseURL: 'https://api.mybebe.net/api/v1/auth'
 })
 
+export const loggedApi = axios.create({
+    baseURL:'https://api.mybebe.net/api/v1/diary',
+    headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${localStorage.getItem('accessToken')}`
+    }
+})
 
+
+loggedApi.interceptors.request.use(
+    function(config) {
+        const accessToken = localStorage.getItem('accessToken')
+        config.headers.Authorization = `Bearer ${accessToken}`
+        return config
+    },
+    function(error) {
+        return Promise.reject(error);
+    }
+)
+
+loggedApi.interceptors.response.use(
+    function(response){
+        return response
+    },
+    function(error){
+        if(error.response === 404){
+            window.location.replace('/notfound')
+        }
+        return Promise.reject(error);
+    }
+)
