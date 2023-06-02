b1ff672872b4d3192337ec88f629a16b187090bb
김혜영 <kimhyeyeong@gimhyeyeong-ui-MacBookAir.local>
2023-06-02 16:22:23 +0900

    User css설정

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -4,9 +4,9 @@ export default function User() {
 
 
     return (
-        <div>
-            <img />
-            <span></span> 
+        <div className='flex items-center'>
+            <img className='w-10 h-10 rounded-full mr-2' />
+            <span className='hidden md:block'>사용자이름</span> 
         </div>
     );
 }
