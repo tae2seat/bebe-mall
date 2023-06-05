456f1001ce2805715c5c163b2f32d52277b1d41b
hye-yeong <ghdwls69@naver.com>
2023-06-05 16:47:27 +0900

    로그인 페이지 설명

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -14,14 +14,15 @@ export default function Login() {
         setPassword(e.target.value)
     }
     const handleSubmitLogin = async (e) => {
-        e.preventDefault();
-        try {
+        e.preventDefault(); // 이벤트의 기본 동작을 중지시키는 메서드, 이룰 호출하면 이벤트에 대한 기본 동작이 실행되지 않고, 원하는 동작 수행 가능 
+        try { //try-catch 문을 사용하면 예기치 않은 상황이 발생할 때 모니커링 할 수 있고 적절하게 처리할 수 있음
             const response = await authApi.post('/login',{
                 email,
                 password
             })
-            console.log('성공!!')
-            //스토리지에 저장하기 
+            //로컬스토리지에 저장하는 이유? 웹페이지를 새로 고침하거나 브라우저가 종료한 후에도 데이터가 유지되게 하기 위해 
+            localStorage.setItem('accessToken', response.data.accessToken)
+            localStorage.setItem('refreshToken', response.data.refreshToken)
         } catch (error) {
             console.log(error)
         }
