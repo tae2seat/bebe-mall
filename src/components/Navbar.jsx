25226c89f5249094fb3b2a8a31646c8fbb8ad972
hye-yeong <ghdwls69@naver.com>
2023-06-05 17:59:35 +0900

    User profile image

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { BsFillPencilFill } from 'react-icons/bs'
 import babyFace from '../images/ICON_08.png'
--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import axios from 'axios';
 
 export default function User() {
+
+    const isLoggedIn = useSelector((state) => state.auth.isLoggedIn)
+
+    const [profileImage, setProfileImage] = useState('')
+
+    useEffect(() => {   
+       if(isLoggedIn) {
+        getProfileImage()
+       }
+    }, [])
+
+    const getProfileImage = async () => {
+        try {
+            const response = await axios.get('https://api.mybebe.net/api/v1/profile',{
+                headers : {
+                    Authorization: `Bearer ${localStorage.getItem('accessToken')}`
+                }
+            })
+            console.log(response.data.avatar)
+            setProfileImage(response.data.avatar)
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
 
     return (
         <div className='flex items-center'>
-            <img className='w-10 h-10 rounded-full mr-2 hidden md:block' />
+            <img 
+                className='w-10 h-10 rounded-full mr-2 hidden md:block' 
+                src={profileImage} 
+                alt='profile' 
+            />
             <span className='hidden md:block'>사용자이름</span> 
         </div>
     );
