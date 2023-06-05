85fe9d9dc7db44507e8d95033554311d6edb2511
hye-yeong <ghdwls69@naver.com>
2023-06-05 23:34:55 +0900

    getProfileImage CORS 해결

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,33 +1,34 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import axios from 'axios';
 import { getProfile } from '../redux/slices/profileSlice';
+import axios from 'axios';
 
 export default function User() {
 
     const dispatch = useDispatch()
 
     const isLoggedIn = useSelector((state) => state.auth.isLoggedIn)
-    const {name, isLoading, isError} = useSelector((state) => state.profile)
+    const { name, isLoading, isError } = useSelector((state) => state.profile)
+
+    const [profileImage, setProfileImage] = useState('')
 
     useEffect(() => {
         if(isLoggedIn){
             dispatch(getProfile())
         }
-    }, [])
-
-    const [profileImage, setProfileImage] = useState('')
+    }, [isLoggedIn])
 
     useEffect(() => {   
-        getProfileImage()
+        fetchProfileImage()
     }, [])
-    // CORS 문제가 일어남 
-    const getProfileImage = async () => {
+
+    //CORS 문제가 일어남  
+    // 백엔드 헤더에 'Cross-origin-Resource-policy' 추가해야 됨
+    const fetchProfileImage = async () => {
         try {
             const response = await axios.get('https://api.mybebe.net/api/v1/profile',{
                 headers : {
                     Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
-                    // "Access-Control-Allow-Origin":"*"
                 }
             })
             console.log(response.data.avatar)
@@ -36,12 +37,20 @@ export default function User() {
             console.log(error)
         }
     }
+    if(isLoading) {
+        return <div>Loading...</div>
+    }
+
+    if(isError) { 
+        return <div>Error occurred!</div>
+    }
+
 
     return (
         <div className='flex items-center'>
             <img 
                 className='w-10 h-10 rounded-full mr-2 hidden md:block' 
-                src={profileImage} 
+                src={profileImage}
                 alt='profile' 
             />
             <span className='hidden md:block'>{name}</span> 
