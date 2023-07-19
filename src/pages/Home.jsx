ff79156419ed492269d0882873cdb3f01db1906f
hye-yeong <ghdwls69@naver.com>
2023-07-19 14:46:03 +0900

    padding

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import Products from '../components/Products';
-import Banner from '../components/Banner';
+import React from "react";
+import Products from "../components/Products";
+import Banner from "../components/Banner";
 
 export default function Home() {
-    return (
-       <>
-            <Banner />
-            <Products />
-       </>
-    );
+  return (
+    <>
+      <Banner />
+      <Products />
+    </>
+  );
 }
-
